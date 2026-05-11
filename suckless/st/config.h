b55944e039b8c02d4a1fc0e542a434dcabf81748
c0mplex <sadasipa@gmail.com>
2026-05-10 19:23:32 -0500

    modifying default cursor

--- suckless/st/config.h
+++ suckless/st/config.h
@@ -168,7 +168,7 @@ static unsigned int defaultrcs = 257;
  * 6: Bar ("|")
  * 7: Snowman ("☃")
  */
-static unsigned int cursorshape = 2;
+static unsigned int cursorshape = 6;
 
 /*
  * Default columns and rows numbers
