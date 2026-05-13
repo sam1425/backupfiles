90b7bfa058d91b83d97562a81935be7e897a440f
c0mplex <sadasipa@gmail.com>
2026-05-12 20:25:20 -0500

    fixing color issue with alpha patch

--- suckless/st/config.h
+++ suckless/st/config.h
@@ -116,7 +116,7 @@ char *stclass = "st";
 unsigned int tabspaces = 4;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 1.0;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
